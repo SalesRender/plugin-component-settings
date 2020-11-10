ce9b907f645478fa894b308a176879782646334f
XAKEPEHOK <icq-475204@ya.ru>
2020-11-10 18:33:44 +0300

    phpdoc fix

--- src/Settings.php
+++ src/Settings.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Created for plugin-component-guzzle
+ * Created for plugin-component-settings
  * Datetime: 11.02.2020 16:49
  * @author Timur Kasumov aka XAKEPEHOK
  */
@@ -8,14 +8,11 @@
 namespace Leadvertex\Plugin\Components\Settings;
 
 
-
 use Leadvertex\Plugin\Components\Db\Model;
 use Leadvertex\Plugin\Components\Form\FormData;
 
 /**
  * Class Settings
- * @package Leadvertex\Plugin\Core\Macros\Models
- *
  * @property $data FormData
  */
 class Settings extends Model
